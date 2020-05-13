6d9fe6580ead093aa453cda50c5d8cd9b7183b75
adityasunny1189 <adityapathak1189@gmail.com>
2020-05-13 17:48:56 +0530

    final commit

---
+++ build/precache-manifest.b35a95033c67ee2ab424f5b5c903d099.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72fc1f896de5841c14c215f12e7e65e0",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "161ab508a338a0c0cf92",
+    "url": "/meme-generator/static/css/main.0049469d.chunk.css"
+  },
+  {
+    "revision": "191c632fcbe60116c02b",
+    "url": "/meme-generator/static/js/2.4fa6336f.chunk.js"
+  },
+  {
+    "revision": "66139b13eaf9141f31fd0ec8d7c80b0c",
+    "url": "/meme-generator/static/js/2.4fa6336f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "161ab508a338a0c0cf92",
+    "url": "/meme-generator/static/js/main.a2e6b1fd.chunk.js"
+  },
+  {
+    "revision": "57ad152325ab189b4b76",
+    "url": "/meme-generator/static/js/runtime-main.c89c2e62.js"
+  }
+]);
